662834b368a276f1d63c87ba91fa733639fe271d
Emmalee <emilee.wimmer@gmail.com>
2022-08-15 16:29:48 -0600

    added button ids and started js vars

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+var currentDay = document.getElementById("currentDay");
+var task = document.querySelector(".description");
+var nineAM = document.getElementById("nineAM");
+var tenAM = document.getElementById("tenAM");
+var elevenAM = document.getElementById("elevenAM");
+var noon = document.getElementById("noon");
+var onePM = document.getElementById("onePM");
+var twoPM = document.getElementById("twoPM");
+var threePM = document.getElementById("threePM");
+var fourPM = document.getElementById("fourPM");
+var fivePM = document.getElementById("fivePM");
+var sixPM = document.getElementById("sixPM");
+var sevenPM = document.getElementById("sevenPM");
+var eightPM = document.getElementById("eightPM");
+var ninePM = document.getElementById("ninePM");
+
+var nineAMbtn = document.getElementById("nineAMbtn");
+var tenAMbtn = document.getElementById("tenAMbtn");
+var elevenAMbtn = document.getElementById("elevenAMbtn");
+var noonBtn = document.getElementById("noonBtn");
+var onePMbtn = document.getElementById("onePMbtn");
+var twoPMbtn = document.getElementById("twoPMbtn");
+var threePMbtn = document.getElementById("threePMbtn");
+var fourPMbtn = document.getElementById("fourPMbtn");
+var fivePMbtn = document.getElementById("fivePM");
+var sixPMbtn = document.getElementById("sixPMbtn");
+var sevenPMbtn = document.getElementById("sevenPMbtn");
+var eightPMbtn = document.getElementById("eightPMbtn");
+var ninePMbtn = document.getElementById("ninePMbtn");
+
+
+// set current time and date counter
+
+// change color of input feilds, .past/.present/.future
+
+// save tasks in local storage
+
+// spit tasks back out from local storage on page reload
+
+
+
+// OPTIONAL: make pin button change color when task is pinned
