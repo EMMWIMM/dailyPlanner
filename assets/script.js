a282ed4d6deb2e5e8c0b36415590a3f158513dcf
Emmalee Anderson <emilee.wimmer@gmail.com>
2022-08-16 20:48:56 -0600

    stuck input into localstorage

--- assets/script.js
+++ assets/script.js
@@ -28,37 +28,82 @@ var sevenPMbtn = document.getElementById("sevenPMbtn");
 var eightPMbtn = document.getElementById("eightPMbtn");
 var ninePMbtn = document.getElementById("ninePMbtn");
 var today = new Date();
-var hours = today.getHours();
+var hour = today.getHours();
 var minutes = today.getMinutes();
 var amPm = " am"
 
+
+
+setColor();
+initializeLocalStorage();
+renderTasksToPage();
+
+
 // $("currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm a"));
 // console.log(today);
-// currentDay.textContent= today;
+currentDay.textContent= today;
 // set current time and date counter
 
-
-
-
-
-
-function setInterval(){
-if(hours > 12){
-  hours = hours-12;
-  amPm = " pm"
-}}
-
 function setColor(){
-  if( hours == 1 && amPm == " pm"){
-    document.onePM.classname = "present";
-  }
-  else if (hours <= 1 || amPm == " am") {
-    document.onePM.classname = "past";
-  }
-  else {
-    document.onePM.classname = "future";
+  console.log('setColor');
+  // if( hours == 1 && amPm == " pm"){
+  //   document.onePM.classname = "present";
+  // }
+  // else if (hours <= 1 || amPm == " am") {
+  //   document.onePM.classname = "past";
+  // }
+  // else {
+  //   document.onePM.classname = "future";
+  // }
+
+  var hourDescs = document.getElementsByClassName("description");
+  for(let i = 0; i < hourDescs.length; ++i){
+    console.log(hourDescs[i].id);
+    var offset = 9;
+    if(hourDescs[i].id.substring(1) < hour){
+        hourDescs[i].classList.add('past');
+    } else if(hourDescs[i].id.substring(1) == hour){
+        hourDescs[i].classList.add('present')
+    } else {
+        hourDescs[i].classList.add('future')
+    }
   }
 }
+var testTasks = ['','','','','',
+            'LUNCH', 'nap','work','go home', 'play games', 'eat dinner', 'go to sleep']
+function renderTasksToPage(){
+  console.log('renderTasksToPage');
+  var tasks = localStorage.getItem('tasks');
+  var hourDescs = document.getElementsByClassName("description");
+    for(let i = 0; i < hourDescs.length; ++i){
+      hourDescs[i].value = tasks[i];
+    }
+
+}
+
+function initializeLocalStorage(){
+  console.log("initializeLocalStorage");
+  //see if there are 'tasks' in localStorage
+
+  //if not create array with '' for each hours
+  var emptyTasks = new Array(document.getElementsByClassName("description").length)
+  localStorage.setItem("tasks", testTasks);
+  console.log(emptyTasks.length);
+}
+
+var tempTasks = localStorage.getItem("tasks");
+function storeTask(id){
+
+  //do stuff
+  console.log('element('+id+') value = '+document.getElementById(id).value);
+  tempTasks [id.substring(1)-9] = document.getElementById(id).value;
+
+  localStorage.setItem("tasks", tempTasks)
+
+
+}
+
+
 // change color of input feilds, .past/.present/.future
 
 // save tasks in local storage
